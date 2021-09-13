1eddc609c637942f4aaa8808c0cdeaf14613976d
Ernesto <ebastidasp@unal.edu.co>
2021-09-12 21:33:13 -0500

    Presentational and Container Components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,36 +2,19 @@ import React, { Component } from 'react';
 import { Media } from 'reactstrap';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
     CardTitle } from 'reactstrap';
+import DishDetail from './DishdetailComponent.js';
+
 
     class Menu extends Component {
 
         constructor(props) {
             super(props);
     
-            this.state = {
-                selectedDish: null
-            }
+            console.log('Menu constructor is invoked');
         }
-    
-        onDishSelect(dish) {
-            this.setState({ selectedDish: dish});
-        }
-    
-        renderDish(dish) {
-            if (dish != null)
-                return(
-                    <Card>
-                        <CardImg top src={dish.image} alt={dish.name} />
-                        <CardBody>
-                          <CardTitle>{dish.name}</CardTitle>
-                          <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                );
-            else
-                return(
-                    <div></div>
-                );
+        
+        componentDidMount(){
+            console.log('Menu componentDidMount is invoked');
         }
     
         render() {
@@ -39,7 +22,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
                 return (
                   <div  className="col-12 col-md-5 m-1">
                     <Card key={dish.id}
-                      onClick={() => this.onDishSelect(dish)}>
+                      onClick={() => this.props.onClick(dish.id)}>
                       <CardImg width="100%" src={dish.image} alt={dish.name} />
                       <CardImgOverlay>
                           <CardTitle>{dish.name}</CardTitle>
@@ -48,16 +31,13 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
                   </div>
                 );
             });
+
+            console.log('Menu render is invoked');
     
             return (
                 <div className="container">
                     <div className="row">
                         {menu}
-                    </div>
-                    <div className="row">
-                      <div  className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.state.selectedDish)}
-                      </div>
                     </div>
                 </div>
             );
